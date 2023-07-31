6220472cc1c69a261ed7df0b5b5d70727629cd62
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-31 18:27:03 +0330

    updating card style on hover

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -9,13 +9,13 @@ const MovieCard = ({ movie }: Props) => {
       <img
         src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
         alt="image"
-        className="w-full h-full rounded-3xl absoloute"
+        className="group-hover:scale-110 transition duration-300 ease-linear w-full  h-full rounded-3xl absoloute"
       />
-      <div className="group-hover:scale-100 card-title transition duration-300 ease">
+      <div className="group-hover:scale-110 card-title transition duration-500 ease">
         <p className="text-md font-bold  mt-10 text-center py-2 px-4">
           {movie.title}
         </p>
-        <button className="bg-blue-700 text-white hover:bg-white hover:text-blue-600 transition-all duration-300 ease-linear text-md px-4 py-2 rounded-xl mb-10">
+        <button className="bg-blue-700 text-white hover:bg-white hover:text-blue-600 font-medium transition-all duration-300 ease-linear text-md px-4 py-2 rounded-xl mb-10">
           See More
         </button>
       </div>
