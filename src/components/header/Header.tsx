664b25dbccbde1b666eb8bce917e85a0e5a4b77f
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-10 12:51:34 +0330

    Refactor - header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,44 +9,55 @@ const Header = () => {
   const { isSidebarOpen, openSidebar } = useSidebarToggleStore();
   const selectedIcon = useSelectedIconStore((s) => s.selectedIcon);
 
-  return (
-    <>
-      <div className="flex flex-row justify-between items-center my-10">
-        <h2 className="text-5xl font-bold">{selectedIcon}</h2>
-        <div className="hidden md:block">
-          <SearchBar />
-        </div>
-        <div className="md:hidden flex flex-row gap-5">
-          {!isSidebarOpen && !isSearchbarOpen && (
-            <button
-              className="relative my-1 mx-auto shadow-lg bg-blue-600 hover:bg-white text-white hover:text-blue-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
-              onClick={() => openSidebar()}
-            >
-              <AiOutlineMenu />
-            </button>
-          )}
-          {!isSearchbarOpen && (
-            <button
-              className="relative my-1 mx-auto shadow-lg bg-blue-600 hover:bg-white text-white hover:text-blue-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
-              onClick={() => openSearchbar()}
-            >
-              <AiOutlineSearch />
-            </button>
-          )}
-          {isSearchbarOpen && (
-            <div className="flex flex-col gap-5 z-50 absolute top-0 left-0 sm:left-32 mx-5">
-              <button
-                className="relative my-1 mx-auto shadow-lg bg-red-600 hover:bg-white text-white hover:text-red-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
-                onClick={() => closeSearchbar()}
-              >
-                <AiOutlineClose />
-              </button>
-              <SearchBar />
-            </div>
-          )}
-        </div>
+  const renderSidebarButton = () => {
+    if (!isSidebarOpen && !isSearchbarOpen) {
+      return (
+        <button
+          className="relative my-1 mx-auto shadow-lg bg-blue-600 hover:bg-white text-white hover:text-blue-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
+          onClick={() => openSidebar()}
+        >
+          <AiOutlineMenu />
+        </button>
+      );
+    }
+    return null;
+  };
+
+  const renderSearchbarButton = () => {
+    if (!isSearchbarOpen) {
+      return (
+        <button
+          className="relative my-1 mx-auto shadow-lg bg-blue-600 hover:bg-white text-white hover:text-blue-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
+          onClick={() => openSearchbar()}
+        >
+          <AiOutlineSearch />
+        </button>
+      );
+    }
+    return (
+      <div className="flex flex-col gap-5 z-50 absolute top-0 left-0 sm:left-32 mx-5">
+        <button
+          className="relative my-1 mx-auto shadow-lg bg-red-600 hover:bg-white text-white hover:text-red-600 text-2xl py-3 px-4 transition-all duration-150 ease-linear cursor-pointer rounded-3xl hover:rounded-xl"
+          onClick={() => closeSearchbar()}
+        >
+          <AiOutlineClose />
+        </button>
+        <SearchBar />
       </div>
-    </>
+    );
+  };
+
+  return (
+    <div className="flex flex-row justify-between items-center my-10">
+      <h2 className="text-5xl font-bold">{selectedIcon}</h2>
+      <div className="hidden md:block">
+        <SearchBar />
+      </div>
+      <div className="md:hidden flex flex-row gap-5">
+        {renderSidebarButton()}
+        {renderSearchbarButton()}
+      </div>
+    </div>
   );
 };
 
