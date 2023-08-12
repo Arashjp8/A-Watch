650aa92f9ae18916ba72e92d04bb841aae3973a3
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-12 15:18:01 +0330

    Refactor - showing sections dynamicly in home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,99 +8,57 @@ import useTrendingMovies from "../hooks/useTrendingMovies";
 import useTrendingTvShows from "../hooks/useTrendingTvShows";
 
 const Home = () => {
-  const {
-    data: movies,
-    isLoading: moviesIsLoading,
-    error: moviesError,
-  } = usePopularMovies();
-
-  const {
-    data: tvShows,
-    isLoading: tvShowsIsLoading,
-    error: tvShowsError,
-  } = usePopularTvShows();
-
-  const {
-    data: trendingMovies,
-    isLoading: trendingMoviesIsLoading,
-    error: trendingMoviesError,
-  } = useTrendingMovies();
-
-  const {
-    data: trendingTvShows,
-    isLoading: trendingTvShowsIsLoading,
-    error: trendingTvShowsError,
-  } = useTrendingTvShows();
-
-  if (
-    moviesIsLoading ||
-    tvShowsIsLoading ||
-    trendingMoviesIsLoading ||
-    trendingTvShowsIsLoading
-  )
-    return (
-      <div className="h-[100vh]">
-        <Spinner />
-      </div>
-    );
-
-  if (
-    moviesError ||
-    tvShowsError ||
-    trendingMoviesError ||
-    trendingTvShowsError
-  )
-    return (
-      <p className="h-[100vh]">
-        {moviesError?.message ||
-          tvShowsError?.message ||
-          trendingMoviesError?.message ||
-          trendingTvShowsError?.message}
-      </p>
-    );
+  const sections = [
+    {
+      title: "Trending Movies",
+      link: "/trending",
+      fetchedData: useTrendingMovies(),
+    },
+    {
+      title: "Trending Tv Shows",
+      link: "/trending",
+      fetchedData: useTrendingTvShows(),
+    },
+    {
+      title: "Movies",
+      link: "/movies",
+      fetchedData: usePopularMovies(),
+    },
+    {
+      title: "Tv Shows",
+      link: "/tvshows",
+      fetchedData: usePopularTvShows(),
+    },
+  ];
 
   return (
     <>
-      <Section
-        title="Trending Movies"
-        link="/trending"
-        selectedIcon="Trending"
-        content={trendingMovies?.pages.slice(0, 1).map((page, index) => (
-          <React.Fragment key={index}>
-            <HorizontalScroll items={page?.results} />
-          </React.Fragment>
-        ))}
-      />
-      <Section
-        title="Trending Tv Shows"
-        link="/trending"
-        selectedIcon="Trending"
-        content={trendingTvShows?.pages.slice(0, 1).map((page, index) => (
-          <React.Fragment key={index}>
-            <HorizontalScroll items={page?.results} />
-          </React.Fragment>
-        ))}
-      />
-      <Section
-        title="Movies"
-        link="/movies"
-        selectedIcon="Movies"
-        content={movies?.pages.slice(0, 1).map((page, index) => (
-          <React.Fragment key={index}>
-            <HorizontalScroll items={page?.results} />
-          </React.Fragment>
-        ))}
-      />
-      <Section
-        title="Tv Shows"
-        link="/tvshows"
-        selectedIcon="Tv Shows"
-        content={tvShows?.pages.slice(0, 1).map((page, index) => (
-          <React.Fragment key={index}>
-            <HorizontalScroll items={page?.results} />
-          </React.Fragment>
-        ))}
-      />
+      {sections.map(({ title, link, fetchedData }) => {
+        const { data: items, isLoading, error } = fetchedData;
+
+        if (isLoading)
+          return (
+            <div key={title} className="h-[100vh]">
+              <Spinner />
+            </div>
+          );
+
+        if (error) return <p className="h-[100vh]">{error.message}</p>;
+
+        return (
+          <Section
+            key={title}
+            title={title}
+            link={link}
+            selectedIcon={title}
+            content={items?.pages.slice(0, 1).map((page, index) => (
+              <React.Fragment key={index}>
+                <HorizontalScroll items={page?.results} />
+              </React.Fragment>
+            ))}
+          />
+        );
+      })}
     </>
   );
 };
