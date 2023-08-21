48ce62c350e5dcd91c10f19925fda00cd92f6336
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-21 19:38:50 +0330

    adding watchlist page

--- src/pages/WatchList.tsx
+++ src/pages/WatchList.tsx
@@ -1,5 +1,20 @@
+import ContentVerticalCard from "../components/content/ContentVerticalCard";
+import useBookmarkStore from "./store";
+
 const WatchList = () => {
-  return <div>watchlist</div>;
+  const { bookmarkedContent } = useBookmarkStore();
+
+  return (
+    <section
+      className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sxl:grid-cols-5 xl:grid-cols-6 gap-5 md:gap-0 ${
+        bookmarkedContent.length < 6 ? "h-[100vh]" : ""
+      }`}
+    >
+      {bookmarkedContent.map((content) => (
+        <ContentVerticalCard key={content.id} data={content} />
+      ))}
+    </section>
+  );
 };
 
 export default WatchList;
