0bbf9c5b988477a149e194e2050f07ede49ee617
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-23 22:31:34 +0330

    fetching person's movies

---
+++ src/hooks/usePersonMovie.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { Movie } from "../interfaces/Movie";
+import { BaseUrl, apiKey } from "../services/config";
+import apiClient from "../services/apiClient";
+
+interface PersonMovies {
+  cast: Movie[];
+}
+
+const usePersonMovie = (personId: number) => {
+  return useQuery<PersonMovies, Error>({
+    queryKey: ["person-movies", personId],
+    queryFn: async () => {
+      return await apiClient(
+        `${BaseUrl}/person/${personId}/movie_credits?api_key=${apiKey}&language=en-US`,
+      ).then((res) => res.data);
+    },
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+  });
+};
+
+export default usePersonMovie;
--- src/pages/Person.tsx
+++ src/pages/Person.tsx
@@ -3,15 +3,26 @@ import Spinner from "../components/Spinner";
 import CastAndCrewCard from "../components/castAndCrew/CastAndCrewCard";
 import useCastAndCrewStore from "../components/castAndCrew/store";
 import usePerson from "../hooks/usePerson";
+import Section from "../components/Section";
+import usePersonMovie from "../hooks/usePersonMovie";
 
 const Person = () => {
   const [isExpand, setExpand] = useState(false);
   const { selectedCastAndCrewId } = useCastAndCrewStore();
-  const { data: person, isLoading, error } = usePerson(selectedCastAndCrewId);
+  const {
+    data: person,
+    isLoading: isPersonLoading,
+    error: personError,
+  } = usePerson(selectedCastAndCrewId);
+  const {
+    data: movies,
+    isLoading: isMoviesLoading,
+    error: moviesError,
+  } = usePersonMovie(selectedCastAndCrewId);
 
-  if (error) return <p className="h-[100vh]">{error.message}</p>;
+  if (personError) return <p className="h-[100vh]">{personError.message}</p>;
 
-  if (isLoading)
+  if (isPersonLoading)
     return (
       <div className="h-[100vh]">
         <Spinner />
@@ -28,35 +39,47 @@ const Person = () => {
         />
         <section className="flex flex-col gap-5">
           <span className="flex flex-row items-center gap-5 font-light">
-            <h3 className="text-base ssm:text-lg font-semibold text-white/60">
+            <h3 className="text-base font-semibold text-white/60 ssm:text-lg">
               Known for:{" "}
             </h3>
             <p className="text-lg text-white">{person.known_for_department}</p>
           </span>
           <span className="flex flex-row items-center gap-5 font-light">
-            <h3 className="text-base ssm:text-lg font-semibold text-white/60">
+            <h3 className="text-base font-semibold text-white/60 ssm:text-lg">
               Birthday:{" "}
             </h3>
             <p className="text-lg text-white">{person.birthday}</p>
           </span>
         </section>
       </div>
-      <section className="">
-        <h3 className="text-xl ssm:text-2xl text-white/60 cursor-pointer font-light pb-3">
-          Biography:{" "}
-        </h3>
-        <p className="text-lg text-white font-light">
-          {isExpand
-            ? person?.biography
-            : person?.biography.slice(0, 1000) + "..."}
-          <button
-            onClick={() => setExpand(!isExpand)}
-            className="text-sm font-semibold my-2 px-4 py-2 rounded-3xl hover:rounded-xl transition-all duration-150 ease-linear flex flex-row gap-1 items-center text-white bg-blue-600 hover:bg-white hover:text-blue-600"
-          >
-            {isExpand ? "Show less" : "Show more"}
-          </button>
-        </p>
-      </section>
+      <div className="flex flex-col">
+        <section className="">
+          <h3 className="cursor-pointer pb-3 text-xl font-light text-white/60 ssm:text-2xl">
+            Biography:{" "}
+          </h3>
+          <p className="text-lg font-light text-white">
+            {isExpand
+              ? person?.biography
+              : person?.biography.length > 1000
+              ? person?.biography.slice(0, 1000) + "..."
+              : person?.biography}
+            <button
+              onClick={() => setExpand(!isExpand)}
+              className={`${
+                person?.biography.length > 1000 ? "block" : "hidden"
+              } my-2 flex flex-row items-center gap-1 rounded-3xl bg-blue-600 px-4 py-2 text-sm font-semibold text-white transition-all duration-150 ease-linear hover:rounded-xl hover:bg-white hover:text-blue-600`}
+            >
+              {isExpand ? "Show less" : "Show more"}
+            </button>
+          </p>
+        </section>
+        <section className="w-full">
+          <h3 className="cursor-pointer pb-3 text-2xl font-light text-white/60 hover:text-white ssm:text-3xl">
+            Movies:
+          </h3>
+          {movies?.cast.map((movie) => <p>{movie.title}</p>)}
+        </section>
+      </div>
     </div>
   );
 };
