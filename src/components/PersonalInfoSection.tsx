6f354cef1662d973d8de0cfeb2ad68494f3db221
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-27 11:59:06 +0330

    Refactor - simpler personal info

--- src/components/PersonalInfoSection.tsx
+++ src/components/PersonalInfoSection.tsx
@@ -9,8 +9,14 @@ const PersonalInfoSection = ({ person, mobileView }: Props) => {
   const gender = person?.gender === 1 ? "Female" : "Male";
   return (
     <>
-      {!mobileView && (
-        <section className={"hidden flex-col gap-1 sm:flex"}>
+      {
+        <section
+          className={`${
+            mobileView
+              ? "flex flex-col gap-2 sm:hidden"
+              : "hidden flex-col gap-1 sm:flex"
+          }`}
+        >
           <h3 className="pb-3 text-xl font-light text-white/60 ssm:text-2xl">
             Personal Info
           </h3>
@@ -27,27 +33,7 @@ const PersonalInfoSection = ({ person, mobileView }: Props) => {
             />
           </div>
         </section>
-      )}
-
-      {mobileView && (
-        <section className="flex flex-col gap-2 sm:hidden">
-          <h3 className="pb-3 text-xl font-light text-white/60 ssm:text-2xl">
-            Personal Info
-          </h3>
-          <div className="flex flex-col gap-5">
-            <PersonalInfo
-              title="Known For"
-              content={person.known_for_department}
-            />
-            <PersonalInfo title="Gender" content={gender} />
-            <PersonalInfo title="Birthday" content={person.birthday} />
-            <PersonalInfo
-              title="Place of Birth"
-              content={person.place_of_birth}
-            />
-          </div>
-        </section>
-      )}
+      }
     </>
   );
 };
