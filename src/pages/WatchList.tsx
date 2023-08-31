068557061727ab9d23ace183e03ddf745e723af6
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-31 18:20:20 +0330

    Fix - Watchlist component bug

--- src/pages/WatchList.tsx
+++ src/pages/WatchList.tsx
@@ -6,12 +6,19 @@ const WatchList = () => {
 
   return (
     <section
-      className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sxl:grid-cols-5 xl:grid-cols-6 gap-5 md:gap-0 ${
+      className={`grid grid-cols-2 gap-5 md:grid-cols-3 md:gap-0 lg:grid-cols-4 sxl:grid-cols-5 xl:grid-cols-6 ${
         bookmarkedContent.length < 6 ? "h-[87vh] sm:h-[100vh]" : ""
       }`}
     >
       {bookmarkedContent.map((content) => (
-        <ContentVerticalCard key={content.id} data={content} />
+        <ContentVerticalCard
+          key={content.id}
+          data={content}
+          size="w-36 h-[440px]"
+          detailTop="top-48"
+          nameFontSize="text-lg"
+          marginBottom="mb-0"
+        />
       ))}
     </section>
   );
