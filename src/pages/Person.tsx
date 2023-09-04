f0e9a91986a2ef7fc620977a8a95c2be2d6efcc9
Arashjp8 <arashjafarpour723@gmail.com>
2023-09-04 14:16:09 +0330

    better person page look

--- src/pages/Person.tsx
+++ src/pages/Person.tsx
@@ -60,42 +60,49 @@ const Person = () => {
     );
 
   return (
-      <div className="flex flex-col gap-10 sm:flex-row">
-        <div className="flex flex-col items-center gap-5">
-          <CastAndCrewCard
-            c={person}
-            imageSize="w-56 h-70"
-            fontSize="text-[26px]"
-            isName={false}
-          />
-          <PersonalInfoSection person={person} mobileView={false} />
-        </div>
-        <div className="flex w-[90%] flex-col gap-8 p-0 sm:pr-20">
-          <p className={`text-2xl font-semibold text-white/80 md:text-3xl`}>
-            {person.name}
-          </p>
-          <PersonalInfoSection person={person} mobileView={true} />
-          <BiographySection biography={person.biography} />
-          {movies && movies.length > 0 && (
-            <Section
-              title="Movies"
-              link=""
-              selectedIcon=""
-              numberOfItems={movies.length}
-              content={<HorizontalScroll items={movies} title="Movies" />}
-            />
-          )}
-          {tvShows && tvShows.length > 0 && (
-            <Section
-              title="Tv Shows"
-              link=""
-              selectedIcon=""
-              numberOfItems={tvShows.length}
-              content={<HorizontalScroll items={tvShows} title="Tv Shows" />}
-            />
-          )}
-        </div>
+    <div className="flex flex-col gap-10 sm:flex-row">
+      <div className="flex flex-col items-center gap-5">
+        <CastAndCrewCard
+          c={person}
+          imageSize="w-40 sm:w-56 sm:h-70"
+          fontSize="text-[26px]"
+          isName={false}
+        />
+        <PersonalInfoSection person={person} mobileView={false} />
+        <p
+          className={`block text-center text-2xl font-semibold text-white/80 sm:hidden sm:text-left md:text-3xl`}
+        >
+          {person.name}
+        </p>
       </div>
+      <div className="flex w-[90%] flex-col gap-8 p-0 sm:pr-20">
+        <p
+          className={`hidden text-center text-2xl font-semibold text-white/80 sm:block sm:text-left md:text-3xl`}
+        >
+          {person.name}
+        </p>
+        <PersonalInfoSection person={person} mobileView={true} />
+        <BiographySection biography={person.biography} />
+        {movies && movies.length > 0 && (
+          <Section
+            title="Movies"
+            link=""
+            selectedIcon=""
+            numberOfItems={movies.length}
+            content={<HorizontalScroll items={movies} title="Movies" />}
+          />
+        )}
+        {tvShows && tvShows.length > 0 && (
+          <Section
+            title="Tv Shows"
+            link=""
+            selectedIcon=""
+            numberOfItems={tvShows.length}
+            content={<HorizontalScroll items={tvShows} title="Tv Shows" />}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
